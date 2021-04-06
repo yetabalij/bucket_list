1f28dd880b823c2aa220a564c510fbbcba8ca1ad
yetabalij <yetabalij@gmail.com>
2021-04-06 15:36:25 +0300

    feach from the front end is done.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,37 @@
 <template>
-  <div>
-    <p>Hello Ermi</p>
+  <div id="app">
+    <div class="notification" v-for="(item, i) in items" :key="item._id">
+      <p>
+        <span class="tag is-primary">{{ i + 1 }}</span>
+        {{ item.description }}
+      </p>
+    </div>
   </div>
 </template>
 
-<script></script>
+<script>
+import axios from "axios";
+export default {
+  name: "App",
+  data() {
+    return {
+      items: [],
+    };
+  },
 
-<style></style>
+  async mounted() {
+    const response = await axios.get(
+      "http://localhost:5000/api/getallbucketlist"
+    );
+    this.items = response.data.bucketlist;
+  },
+};
+</script>
+
+<style>
+#app {
+  margin: auto;
+  margin-top: 3rem;
+  max-width: 700px;
+}
+</style>
